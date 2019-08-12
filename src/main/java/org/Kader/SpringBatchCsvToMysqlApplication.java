19c3e8263853cf8f6a36d46e3efb2d6832b331e1
Yahyaoui <yahyaouiabdelkader85@gmail.com>
2019-08-12 18:42:18 +0200

    first commit

---
+++ src/main/java/org/Kader/SpringBatchCsvToMysqlApplication.java
@@ -0,0 +1,13 @@
+package org.Kader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchCsvToMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchCsvToMysqlApplication.class, args);
+	}
+
+}
